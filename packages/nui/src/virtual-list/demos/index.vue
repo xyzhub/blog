2bd18f06d389c2e0f8e9d69b8f178b5481171580
wq <3479304511@qq.com>
2024-03-14 16:43:35 +0800

    feat: 完成虚拟列表基础功能

--- packages/nui/src/virtual-list/demos/index.vue
+++ packages/nui/src/virtual-list/demos/index.vue
@@ -1,9 +1,16 @@
 <script setup lang='ts'>
-
+const data = Array(1000).fill(0).map((_, index) => ({
+  id: index,
+  name: `name${index}`,
+  age: index,
+  sex: index % 2 === 0 ? '男' : '女',
+  address: `address${index}`,
+  phone: '1234567890',
+}))
 </script>
 
 <template>
-  <n-virtual-list />
+  <n-virtual-list :data="data" />
 </template>
 
 <style scoped lang='less'>
